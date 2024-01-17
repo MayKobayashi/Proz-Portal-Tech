a25396e7b8d71691b60c3654e3ae883a39b879af
May <145486840+MayKobayashi@users.noreply.github.com>
2024-01-16 21:28:26 -0300

    Add files via upload

---
+++ Projeto 3 - Introdução à Programação.py
@@ -0,0 +1,23 @@
+#numero = 0
+#for i in range(20):
+#    numero = numero + 1
+#    if(numero == 13):
+#        continue
+#    else:
+#        print(numero)
+
+#numero = 1
+#while(numero <= 20):
+#    if(numero == 13):
+#        numero = numero + 1
+#        continue
+#    else:
+#        print(numero)
+#        numero = numero + 1
+
+numero = 21
+for i in range(20, 0, -1):
+    numero = numero - 1
+    if(numero == 13):
+        continue
+    print(numero)
