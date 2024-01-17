8c78f2e9cda17081badbe1bbd5792b0d1ea9b120
May <145486840+MayKobayashi@users.noreply.github.com>
2024-01-16 21:29:25 -0300

    Update Projeto 3 - Introdução à Programação.py

--- Projeto 3 - Introdução à Programação.py
+++ Projeto 3 - Introdução à Programação.py
@@ -1,19 +1,33 @@
-#numero = 0
-#for i in range(20):
-#    numero = numero + 1
-#    if(numero == 13):
-#        continue
-#    else:
-#        print(numero)
+#Instruções do projeto
+#Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.
 
-#numero = 1
-#while(numero <= 20):
-#    if(numero == 13):
-#        numero = numero + 1
-#        continue
-#    else:
-#        print(numero)
-#        numero = numero + 1
+#Escreva um código que imprima todos os números exceto o número 13.
+#Escreva mais um código que resolva o mesmo problema, mas dessa vez usando o laço de repetição 'while'.
+
+#Como desafio, imprima eles em ordem decrescente (20, 19, 18...)
+
+#Imprimindo os números utilizando for:
+
+numero = 0
+for i in range(20):
+    numero = numero + 1
+    if(numero == 13):
+        continue
+    else:
+        print(numero)
+
+#Imprimindo os números utilizando while:
+
+numero = 1
+while(numero <= 20):
+    if(numero == 13):
+        numero = numero + 1
+        continue
+    else:
+        print(numero)
+        numero = numero + 1
+
+#Imprimindo os números em ordem decrescente:
 
 numero = 21
 for i in range(20, 0, -1):
